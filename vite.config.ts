e576f4f709be3519e9ceb77ebf1e8095e84e79cd
Leon de Klerk <developer@leondeklerk.com>
2024-03-29 02:13:06 +0100

    Example of building a library with multiple entries
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from "node:url";
-
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -9,6 +9,30 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	build: {
+		lib: {
+			entry: [
+				resolve(__dirname, "src/main.ts"),
+				resolve(__dirname, "src/views/views.ts"),
+				resolve(__dirname, "src/stores/stores.ts"),
+				resolve(__dirname, "src/components/components.ts"),
+			],
+			name: "LeonLibrary",
+			fileName: (_format, name) => {
+				return `${name}.js`;
+			},
+			formats: ["es"],
+		},
+		rollupOptions: {
+			external: ["vue", "pinia", "vue-router"],
+			output: {
+				preserveModules: true,
+				globals: {
+					vue: "Vue",
+				},
+			},
 		},
 	},
 	server: {
